31dc50c1302026c77434074d339279806e528f0b
Jakob Krarup <kr@rup.dk>
2016-11-28 11:09:00 +0100

    Stats only visible on active player's units

--- js/Player.js
+++ js/Player.js
@@ -87,23 +87,27 @@
         unit.attack = attack;
         unit.defense = defense;
         unit.movesPerTurn = movement;
-
+        unit.player = this;
         var style = { align: "center", fontSize: 18, fill: "white", stroke: "black", strokeThickness: 2 };
 
         var statText = unit.attack + "/" + unit.defense + "/" + unit.movesPerTurn;
-        var stats = game.add.text(TileSize/2, 0 -25, statText, style);
-        stats.setShadow(3, 3, 'rgba(0,0,0,1)', 5);
-        stats.anchor.set(.5, 0);
-        unit.addChild(stats);
+        unit.stats = game.add.text(TileSize/2, 0 -25, statText, style);
+        unit.stats.setShadow(3, 3, 'rgba(0,0,0,1)', 5);
+        unit.stats.anchor.set(.5, 0);
+        unit.addChild(unit.stats);
             
         unit.movesLeftText = game.add.text(TileSize / 2, TileSize+3, statText, style);
         unit.movesLeftText.setShadow(3, 3, 'rgba(0,0,0,1)', 5);
         unit.movesLeftText.anchor.set(.5, 0);
         unit.addChild(unit.movesLeftText);
+        
 
         unit.update = function ()
         {
             this.movesLeftText.text = this.movesLeft + " moves";
+            var visibleStats = (game.getCurrentPlayer() == this.player);
+            this.stats.visible = visibleStats;
+            this.movesLeftText.visible = visibleStats;
         }
         
         unit.movesLeft = unit.movesPerTurn;
